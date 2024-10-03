30c3b4563db9ae7facefc1438271bd1960b70023
cattrh <olechkaarustamova@gmail.com>
2024-10-03 21:43:57 +0300

    Lessons3

---
+++ Homework_3.py
@@ -0,0 +1,15 @@
+a = int(input("Введите число от 1 до 100: "))
+sum = 0
+for i in range(1, a+1):
+  sum += i**3
+print(sum)
+
+b = []
+for i in range(1,10):
+  for j in range(1,10):
+    if i*j < 10:
+      b.append(" "+str(i*j))
+    else:
+      b.append(str(i*j))
+  print(*b)
+  b = []
