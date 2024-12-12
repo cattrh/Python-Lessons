154c66062a408f08a196465fab28614e3c3eea6d
cattrh <olechkaarustamova@gmail.com>
2024-12-12 10:20:15 +0300

    Lessons8

--- Homework_8.py
+++ Homework_8.py
@@ -13,3 +13,7 @@ def find_abbreviations(text):
 text = "ФГУП НИЦ ГИДГЕО, ФГОУ ЧШУ АПК и т.п."
 abbreviations = find_abbreviations(text)
 print(abbreviations)
+import re
+s='Владимир устроился на работу в одно очень важное место. И в первом же документе он ничего не понял, там были сплошные ФГУП НИЦ ГИДГЕО, ФГОУ ЧШУ АПК и т.п. '
+x=re.findall(r'\b[А-ЯЁ][А-ЯЁ ]*[А-ЯЁ]\b',s)
+print(x)
